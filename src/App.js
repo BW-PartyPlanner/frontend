fafe5d404630e9738e8eb42d11575705682733ed
Tara Gamache <tgamache.13@gmail.com>
2019-11-18 12:05:30 -0800

    basic pot luck form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import FormikPotLuckForm from './components/PotLuckForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <h1>Hello World!</h1>
+      <FormikPotLuckForm />
     </div>
   );
 }
