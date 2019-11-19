daf38f51331bfe42a6e7dc31955f14c8b1a211ed
ndurbin <nick.durbin@gmail.com>
2019-11-19 13:40:51 -0500

    Created a loginUser function with a get request.

--- src/store/actions/loginActions.js
+++ src/store/actions/loginActions.js
@@ -1,3 +1,21 @@
+import { axiosWithoutAuth as axios } from '../../utils/axiosUtils';
+
 export const FETCH_LOGIN_REQUEST = 'FETCH_LOGIN_REQUEST'
 export const FETCH_LOGIN_SUCCESS = 'FETCH_LOGIN_SUCCESS'
 export const FETCH_LOGIN_ERROR = 'FETCH_LOGIN_ERROR'
+
+export const loginUser = () => dispatch => {
+  dispatch({ type: FETCH_LOGIN_REQUEST })
+
+  axios()
+    .get('/users/register', )
+    .then(res => {
+      console.log('data', res.data)
+      dispatch({ type: FETCH_LOGIN_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      console.log("Unable to create user.", err.response.error)
+      dispatch({ type: FETCH_LOGIN_ERROR })
+    })
+
+}
