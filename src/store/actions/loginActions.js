49f7954071a1c811ad514849e22725c6c8327053
nick.durbin <nick.durbin@gmail.com>
2019-11-19 15:10:04 -0500

    Able to successfully login and save token.

--- src/store/actions/loginActions.js
+++ src/store/actions/loginActions.js
@@ -4,17 +4,18 @@ export const FETCH_LOGIN_REQUEST = 'FETCH_LOGIN_REQUEST'
 export const FETCH_LOGIN_SUCCESS = 'FETCH_LOGIN_SUCCESS'
 export const FETCH_LOGIN_ERROR = 'FETCH_LOGIN_ERROR'
 
-export const loginUser = () => dispatch => {
+export const loginUser = user => dispatch => {
   dispatch({ type: FETCH_LOGIN_REQUEST })
 
   axios()
-    .get('/auth/login', )
+    .post('/auth/login', user)
     .then(res => {
       console.log('data', res.data)
       dispatch({ type: FETCH_LOGIN_SUCCESS, payload: res.data })
+      localStorage.setItem("token", res.data.token)
     })
     .catch(err => {
-      console.log("Unable to login.", err.response.error)
+      console.log("Unable to login.", err.response.message)
       dispatch({ type: FETCH_LOGIN_ERROR })
     })
 
