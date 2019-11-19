cf01847d68dd15dbd1a9fa2ffe8969143244951e
ndurbin <nick.durbin@gmail.com>
2019-11-19 13:40:11 -0500

    Added a route to the signup form for testing.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom';
+import SignUpForm from './SignUpForm';
 
 const Navigation = () => {
     return (
@@ -21,6 +22,7 @@ const Navigation = () => {
                     Profile
                 </Link>
             </div>
+            <Route path='/signup' component={SignUpForm} />
         </div>
     );
 };
