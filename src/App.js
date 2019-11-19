2c008385d7c373921debdd8ff95815f3cde42bb3
nick.durbin <nick.durbin@gmail.com>
2019-11-18 20:12:54 -0500

    Added some thing to render in the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <h1>Hello World!</h1>
     </div>
   );
 }
