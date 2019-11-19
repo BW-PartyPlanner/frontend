c359a1e64fa2dd7c18803f2f1d1435a1b69e44c9
nick.durbin <nick.durbin@gmail.com>
2019-11-19 15:09:52 -0500

    Able to successfully create users.

--- src/components/signUpForm.js
+++ src/components/signUpForm.js
@@ -11,27 +11,27 @@ const UserForm = ({ values, touched, errors, status }) => {
             <Form>
                 <Field
                     type="text"
-                    name="firstName"
+                    name="first_name"
                     placeholder="First Name"
-                    value={values.firstName}
+                    value={values.first_name}
                 />
-                {touched.firstName && errors.firstName && <p>{errors.firstName}</p>}
+                {touched.first_name && errors.first_name && <p>{errors.first_name}</p>}
                 
                 <Field
                     type="text"
-                    name="lastName"
+                    name="last_name"
                     placeholder="Last Name"
-                    value={values.lastName}
+                    value={values.last_name}
                 />
-                {touched.lastName && errors.lastName && <p>{errors.lastName}</p>}
+                {touched.last_name && errors.last_name && <p>{errors.last_name}</p>}
 
                 <Field
                     type="text"
-                    name="email"
-                    placeholder="Email"
-                    value={values.email}
+                    name="username"
+                    placeholder="Username"
+                    value={values.username}
                 />
-                {touched.email && errors.email && <p>{errors.email}</p>}
+                {touched.username && errors.username && <p>{errors.username}</p>}
 
                 <Field
                     type="password"
@@ -48,19 +48,19 @@ const UserForm = ({ values, touched, errors, status }) => {
 };
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({firstName, lastName, email, password}) {
+    mapPropsToValues({first_name, last_name, username, password}) {
         return {
-            firstName: firstName || "",
-            lastName: lastName || "",
-            email: email || "",
+            first_name: first_name || "",
+            last_name: last_name || "",
+            username: username || "",
             password: password || ""
         };
     },
 
     validationSchema: Yup.object().shape({
-        firstName: Yup.string().required("What is your first name?"),
-        lastName: Yup.string().required("What is your last name?"),
-        email: Yup.string().required("What is your email?"),
+        first_name: Yup.string().required("What is your first name?"),
+        last_name: Yup.string().required("What is your last name?"),
+        username: Yup.string().required("What is your username?"),
         password: Yup.string().required("Please enter a password.")
     }),
 
