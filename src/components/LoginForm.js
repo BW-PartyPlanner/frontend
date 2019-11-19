7cdd9614f2cfd5ba55bcbf4df8cfdeed36951717
ndurbin <nick.durbin@gmail.com>
2019-11-19 14:02:56 -0500

    Brought in the code to modify from Zack's repo.

---
+++ src/components/LoginForm.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { connect } from 'react-redux';
+import { loginUser } from '../store/actions/loginActions';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const LoginForm = ({ values, touched, errors, status }) => {
+
+    return (
+        <div className="user-form">
+            <Form>
+                <Field
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    value={values.email}
+                />
+                {touched.email && errors.email && <p>{errors.email}</p>}
+
+                <Field
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={values.password}
+                />
+                {touched.password && errors.password && <p>{errors.password}</p>}
+
+                <button type="submit">Submit</button>
+            </Form>     
+        </div>
+    );
+};
+
+const FormikUserForm = withFormik({
+    mapPropsToValues({email, password}) {
+        return {
+            email: email || "",
+            password: password || ""
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        email: Yup.string().required("Please enter an email."),
+        password: Yup.string().required("Password incorrect.")
+    }),
+
+    handleSubmit(values, formikBag) {
+      formikBag.props.loginUser(values);
+      formikBag.resetForm()
+      formikBag.setStatus()
+  }
+})(LoginForm);
+
+export default connect(null, { loginUser })(FormikUserForm);
