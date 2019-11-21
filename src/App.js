db582c0c049b168ec5b8241051ef887c1571367b
Tara Gamache <tgamache.13@gmail.com>
2019-11-20 16:44:48 -0800

    added fonts to imports file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import './styles.scss';
 
+import FormikPotLuckForm from './components/PotLuck/PotLuckForm';
+
 function App() {
 
   return (
     <div className="App">
-      <Navigation />     
+      <Navigation />  
+      <FormikPotLuckForm />   
     </div>
   );
 }
