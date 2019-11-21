c7d83495bd30027907d32701e16d51faafbea297
ndurbin <nick.durbin@gmail.com>
2019-11-21 03:50:39 -0500

    Added the userReducer to the combineReducers.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,5 +3,6 @@ import { reducer as signupReducer } from './signupReducer';
 import { reducer as loginReducer } from './loginReducer';
 import { reducer as itemReducer } from './itemReducer';
 import { reducer as partyReducer } from './partyReducer';
+import { reducer as userReducer } from './userReducer';
 
-export default combineReducers({ signupReducer, loginReducer, itemReducer, partyReducer })
+export default combineReducers({ signupReducer, loginReducer, itemReducer, partyReducer, userReducer })
