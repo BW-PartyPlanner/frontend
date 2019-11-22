866dc3cd13b8acb4369be797768ff59c893de428
Tara Gamache <tgamache.13@gmail.com>
2019-11-21 17:20:15 -0800

    fixed conflict

--- src/components/HostedParty/HostedParty.js
+++ src/components/HostedParty/HostedParty.js
@@ -1,11 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';
-import GuestList from '../Guests/GuestList';
-=======
 import GuestListForm from '../GuestList/GuestListForm';
 
->>>>>>> 2788c4f99c44a90dba22db976986ed74678393da
 
 export default function HostedParty(props) {
 
