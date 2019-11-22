e3b18a5110bc6c9ffed6c70e2a4fd8f96d60a445
nick.durbin <nick.durbin@gmail.com>
2019-11-21 19:24:49 -0500

    Some changes were changes with the latest merge. Added the welcome message back.

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -1,52 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Route } from 'react-router-dom'
 import CreatePartyModal from '../CreatePartyModal';
 import { axiosWithAuth as axios } from '../../utils/axiosUtils';
 
 const Dashboard = (props) => {
-    const [users, setUsers] = useState([])
-    const [user, setUser] = useState({
-        id: '',
-        first_name: '',
-        last_name: '',
-        username: ''
-    })
+    const welcome = localStorage.getItem('message')
+    const [welcomeMessage, setWelcomeMessage] = useState(welcome)
 
     useEffect(() => {
-        axios()
-            .get(`/users/`)
-            .then(res => {
-                console.log(res.data)
-                setUsers(res.data)
-            })
-            .catch(err => {
-                console.log(err, 'User not found.')
-            })
-    }, [])
-
-    console.log(props)
-
-    useEffect(() => {
-        axios()
-            .get(`/users/${props.match.params.id}`)
-            .then(res => {
-                console.log(res.data)
-                setUser(res.data)
-            })
-            .catch(err => {
-                console.log(err, 'User not found.')
-            })
-    }, [props.match.params.id])
+        setWelcomeMessage(welcomeMessage)
+    }, [welcomeMessage])
 
     return (
         <div className="container-dashboard">
-            <h2 key={user.id}>Hello, {user.username}</h2>
+            <h2>{welcomeMessage}</h2>
             <CreatePartyModal />
-            {/* <button>
-                <Link to="/home">
-                    Create Party
-                </Link>
-            </button> */}
             <div className="container-options">
                 <div className="option">My Parties</div>
                 <div className="option">Friends List</div>
