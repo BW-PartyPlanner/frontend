3a5d903b00fd193ee0ce7e4e8e3c24ae57154195
ndurbin <nick.durbin@gmail.com>
2019-11-22 04:34:40 -0500

    Working on the displaying a list of items.

--- src/components/Items/ItemList.js
+++ src/components/Items/ItemList.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { getItems } from '../../store/actions/itemActions';
+import { useDispatch, useSelector } from 'react-redux';
+import Item from './Item';
+import ItemForm from './ItemForm';
 
 function ItemList() {
-  const [items, setItems] = useState([])
+  const dispatch = useDispatch()
+  const items = useSelector(state => state.items)
+  console.log(items)
 
   useEffect(() => {
-    
-  })
+    dispatch(getItems())
+  }, [dispatch])
 
   return (
-    <div>
-      
+    <div className='listContainer'>
+      <div className="formContainer">
+        <ItemForm />
+      </div>
+      <div className='listItems'>
+        {items.map(item => <Item key={item.id} item={item} />)}
+      </div>
     </div>
   )
 }
 
-export default ItemList
+export default ItemList;
