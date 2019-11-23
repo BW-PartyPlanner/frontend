6abde19b7c22922801effead352946af0898006d
ndurbin <nick.durbin@gmail.com>
2019-11-23 07:40:12 -0500

    Made changes to allow for styling.

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -15,19 +15,21 @@ const Dashboard = (props) => {
     }, [welcomeMessage])
 
     return (
-        <div className="container-dashboard">
-            <h2>{welcomeMessage ? welcomeMessage : 'Welcome to the Party!'}</h2>
-            <CreatePartyModal />
-            <div className="container-options">
-                <NavLink to='/dashboard/myparties' className='option'>My Parties</NavLink>
-                <NavLink to='/dashboard/friends' className='option'>Friends List</NavLink>
-                <NavLink to='/dashboard/invitations' className='option'>Invitations</NavLink>
-            </div>
-            <div className="container-dashboard-lower-part">
-                <Route exact path='/dashboard' component={DashboardHome} />
-                <Route path='/dashboard/friends' component={FriendsList} />
-                <Route path='/dashboard/invitations' component={LoginForm} />
-                <Route path='/dashboard/myparties' component={MyParties} />
+        <div className="dashboardWrapper">
+            <div className="container-dashboard">
+                <h2>{welcomeMessage ? welcomeMessage : 'Welcome to the Party!'}</h2>
+                <CreatePartyModal />
+                <div className="container-options">
+                    <NavLink to='/dashboard/myparties' className='option'>My Parties</NavLink>
+                    <NavLink to='/dashboard/friends' className='option'>Friends List</NavLink>
+                    <NavLink to='/dashboard/invitations' className='option'>Invitations</NavLink>
+                </div>
+                <div className="container-dashboard-lower-part">
+                    <Route exact path='/dashboard' component={DashboardHome} />
+                    <Route path='/dashboard/friends' component={FriendsList} />
+                    <Route path='/dashboard/invitations' component={LoginForm} />
+                    <Route path='/dashboard/myparties' component={MyParties} />
+                </div>
             </div>
         </div>
     );
