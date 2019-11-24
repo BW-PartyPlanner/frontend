cc2cb4aad48e221e0db6d1652dc21611ae419c09
Tara Gamache <tgamache.13@gmail.com>
2019-11-24 05:46:18 -0800

    redid styling on potluck party, started hosted party styling, imported accounted for and items list to hosted party

--- src/components/HostedParty/HostedParty.js
+++ src/components/HostedParty/HostedParty.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GuestList from '../GuestList/GuestList';
+import ItemList from '../Items/ItemList';
+import AccountedForList from '../AccountedFor/AccountForList';
 
 export default function HostedParty(props) {
 
     return (
-        <div className="Pot-Luck-Party">
-            <h1>props.Name of Party</h1>
+        <div className="hp-wrapper">
+            <div className="hp-container">
+                <div className="hp-name">
+
+                    <h1 className="hp-party-name">Name of Party</h1>
+                    <button className="hp-edit-button">Edit</button>
+                </div>
+
+                
             
-            
-            
-            <button>Edit</button>
-            
-          
-            <div>
-                <GuestList />
-                <Link to="/guestListForm">
-                    <button>Add Guests</button>
-                </Link>
+                <div className="hp-guest-list">
+                    <div className="hp-gst-list">
+
+                        <GuestList />
+                    </div>
+                    <div className="hp-link-gst-list">
+
+                        <Link to="/guestListForm">
+                            <button className="hp-gst-list-button">Add Guests</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="hp-budget">
+                    
+                    <div className="hp-bdgt">Budget</div>
+                    <div className="hp-rem-bdgt">Remaining Budget</div>
+                </div>
+                <div className="hp-items-list">
+
+                    <h3 className="hp-item-list-title">Items Needed List</h3>
+                    <ItemList />
+                </div>
+                <div className="hp-add-item-link">
+                    <Link to='/itemForm' id="hp-item-link">
+                        <button className="hp-add-items-button">Add Items to List</button>
+                    </Link>
+                </div>
+                <div className="hp-acct-for-list">
+                    <h3 className="hp-acct-for-title">Items Accounted For</h3>
+                    <AccountedForList />
+                </div>
+                <div className="hp-acct-for-link">
+                    <Link to='/accountedForForm' id="af-link">
+                        <button className="hp-acct-for-button">Add Items Accounted For</button>
+                    </Link>
+                </div>
+                
             </div>
-            <div>props.Budget</div>
-            <div>props.Remaining Budget</div>
-            <div>props.Items/Food Needed</div>
-            <div>items accounted for</div>
 
          </div>
     );
--- src/components/PotLuck/PotLuckParty.js
+++ src/components/PotLuck/PotLuckParty.js
@@ -10,66 +10,67 @@ function PotLuckParty(props) {
 
     
     return (
-        <div className="pl-pot-luck-party">
-            <div className="pl-name-div">
+        <div className="pl-wrapper">
+            <div className="pl-container" >
 
-                
+                <div className="pl-name-div">
 
                     <h1 className="pl-pot-luck-name">
                         {props.state.partyReducer.party ? `hello${props.state.partyReducer.party.party.name}` : "Party's Name"}
                     </h1>
-                <div className="pl-guest-num">props.Number of Guests</div>
-            </div>
-            <div className="pl-date-time-location">
-                 <h3>Date of Party</h3>
-                 <p className="pl-time">{props.state.partyReducer.party ? `${props.state.partyReducer.party.party.date}`: "date not available"}</p>
-            </div>
-            <button className="pl-edit-button">Edit</button>
-            
-            <div className="pl-guest-info  ">
-                <div className="pl-gst-list">
+                    <h2 className="pl-guest-num">Number of Guests</h2>
+                    <h3 className="pl-date">Date of Party</h3>
+                    <p className="pl-time">{props.state.partyReducer.party ? `${props.state.partyReducer.party.party.date}`: "date not available"}</p>
 
-                    <GuestList />
+                    <button className="pl-edit-button">Edit</button>
                 </div>
-                <div className="pl-link-to-form">
+               
+                <div className="pl-guest-info  ">
+                    <div className="pl-gst-list">
 
-                    <Link to='/guestListForm' id="pl-guest-link">
-                        <button className="pl-add-guest-button">Add to Guest List</button>
-                    </Link>
-                </div>
-            </div>
-            
-            <div className="pl-lists">
-
-                <div className="pl-item-list">
-                    <h3 className="item-list-title">Items Needed List</h3>
-                    <div className="pl-item-list-div">
-
-                        <ItemList />
+                        <GuestList />
                     </div>
-                    <div className="pl-add-items-link">
+                    <div className="pl-link-to-form">
 
-                        <Link to='/itemForm' id="pl-item-link">
-                            <button className="pl-add-items-button">Add Items to List</button>
+                        <Link to='/guestListForm' id="pl-guest-link">
+                            <button className="pl-add-guest-button">Add to Guest List</button>
                         </Link>
                     </div>
                 </div>
-                <div className="pl-acct-for-list"> 
-                    <div className="pl-acct-for">
-                        
-                        <AccountedForList />  
-                    </div>
-                    <div classname="pl-acct-for-link">
-                    <div className="pl-fix-link-div">
+                
+                
+                
+                <div className="pl-lists">
 
-                        <Link to='/accountedForForm' id="af-link">
-                            <button className="pl-add-acct-for-button">Add Items Accounted For</button>
-                        </Link>
+                    <div className="pl-item-list">
+                        <h3 className="item-list-title">Items Needed List</h3>
+                        <div className="pl-item-list-div">
+
+                            <ItemList />
+                        </div>
+                        <div className="pl-add-items-link">
+
+                            <Link to='/itemForm' id="pl-item-link">
+                                <button className="pl-add-items-button">Add Items to List</button>
+                            </Link>
+                        </div>
                     </div>
+                    <div className="pl-acct-for-list"> 
+                        <div className="pl-acct-for">
+                            
+                            <AccountedForList />  
+                        </div>
+                        <div classname="pl-acct-for-link">
+                        <div className="pl-fix-link-div">
+
+                            <Link to='/accountedForForm' id="af-link">
+                                <button className="pl-add-acct-for-button">Add Items Accounted For</button>
+                            </Link>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
         
         
