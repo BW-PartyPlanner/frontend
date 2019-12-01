f1be9e536e820d83e32985cf2b81e62e581091c7
ndurbin <nick.durbin@gmail.com>
2019-11-30 23:37:47 -0500

    Added a key value to amke the list unique.

--- src/components/MyParties/Party.js
+++ src/components/MyParties/Party.js
@@ -7,7 +7,7 @@ console.log(props, "from party");
     return (
     <div className="rendered-parties">
         {props.parties.map((party) =>
-         <div className="party-link">
+         <div className="party-link" key={party.id}>
             <Link to={{pathname: `/potluckparty/${party.id}`, state:{id: party.id}}}>{party.name}</Link>          
         </div> )}
     </div>
